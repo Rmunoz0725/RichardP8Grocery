5423ec87b6d115e351fcbd159ef624d0d68fddd6
Rmunoz0725 <Richardmunoz25@hotmail.com>
2024-09-16 11:59:06 -0400

    Added new code and erased some text boxes.

--- RichardP8Grocery/Form1.Designer.cs
+++ RichardP8Grocery/Form1.Designer.cs
@@ -31,11 +31,9 @@
             label1 = new Label();
             lblItemCost = new Label();
             txtItemCost = new TextBox();
-            lvlNumofItems = new Label();
+            lblNumofItems = new Label();
             txtNumofItems = new TextBox();
             lstOutput = new ListBox();
-            lblTaxRate = new Label();
-            txtTaxRate = new TextBox();
             btnCalcPrice = new Button();
             btnClear = new Button();
             btnQuit = new Button();
@@ -68,15 +66,15 @@
             txtItemCost.Size = new Size(146, 23);
             txtItemCost.TabIndex = 2;
             // 
-            // lvlNumofItems
+            // lblNumofItems
             // 
-            lvlNumofItems.AutoSize = true;
-            lvlNumofItems.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            lvlNumofItems.Location = new Point(53, 123);
-            lvlNumofItems.Name = "lvlNumofItems";
-            lvlNumofItems.Size = new Size(103, 15);
-            lvlNumofItems.TabIndex = 3;
-            lvlNumofItems.Text = "Number of Items";
+            lblNumofItems.AutoSize = true;
+            lblNumofItems.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblNumofItems.Location = new Point(53, 123);
+            lblNumofItems.Name = "lblNumofItems";
+            lblNumofItems.Size = new Size(103, 15);
+            lblNumofItems.TabIndex = 3;
+            lblNumofItems.Text = "Number of Items";
             // 
             // txtNumofItems
             // 
@@ -89,55 +87,41 @@
             // 
             lstOutput.FormattingEnabled = true;
             lstOutput.ItemHeight = 15;
-            lstOutput.Location = new Point(53, 232);
+            lstOutput.Location = new Point(53, 194);
             lstOutput.Name = "lstOutput";
-            lstOutput.Size = new Size(694, 154);
+            lstOutput.Size = new Size(694, 169);
             lstOutput.TabIndex = 7;
             lstOutput.TabStop = false;
             // 
-            // lblTaxRate
-            // 
-            lblTaxRate.AutoSize = true;
-            lblTaxRate.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            lblTaxRate.Location = new Point(53, 181);
-            lblTaxRate.Name = "lblTaxRate";
-            lblTaxRate.Size = new Size(55, 15);
-            lblTaxRate.TabIndex = 5;
-            lblTaxRate.Text = "Tax Rate";
-            // 
-            // txtTaxRate
-            // 
-            txtTaxRate.Location = new Point(182, 178);
-            txtTaxRate.Name = "txtTaxRate";
-            txtTaxRate.Size = new Size(146, 23);
-            txtTaxRate.TabIndex = 6;
-            // 
             // btnCalcPrice
             // 
-            btnCalcPrice.Location = new Point(78, 406);
+            btnCalcPrice.Location = new Point(78, 393);
             btnCalcPrice.Name = "btnCalcPrice";
             btnCalcPrice.Size = new Size(122, 23);
             btnCalcPrice.TabIndex = 8;
             btnCalcPrice.Text = "Calculate Final &Price";
             btnCalcPrice.UseVisualStyleBackColor = true;
+            btnCalcPrice.Click += btnCalcPrice_Click;
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(360, 406);
+            btnClear.Location = new Point(360, 393);
             btnClear.Name = "btnClear";
             btnClear.Size = new Size(92, 23);
             btnClear.TabIndex = 9;
             btnClear.Text = "&Clear";
             btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
             // 
             // btnQuit
             // 
-            btnQuit.Location = new Point(653, 406);
+            btnQuit.Location = new Point(651, 393);
             btnQuit.Name = "btnQuit";
             btnQuit.Size = new Size(75, 23);
             btnQuit.TabIndex = 10;
             btnQuit.Text = "&Quit";
             btnQuit.UseVisualStyleBackColor = true;
+            btnQuit.Click += btnQuit_Click;
             // 
             // Form1
             // 
@@ -147,11 +131,9 @@
             Controls.Add(btnQuit);
             Controls.Add(btnClear);
             Controls.Add(btnCalcPrice);
-            Controls.Add(txtTaxRate);
-            Controls.Add(lblTaxRate);
             Controls.Add(lstOutput);
             Controls.Add(txtNumofItems);
-            Controls.Add(lvlNumofItems);
+            Controls.Add(lblNumofItems);
             Controls.Add(txtItemCost);
             Controls.Add(lblItemCost);
             Controls.Add(label1);
@@ -166,11 +148,9 @@
         private Label label1;
         private Label lblItemCost;
         private TextBox txtItemCost;
-        private Label lvlNumofItems;
+        private Label lblNumofItems;
         private TextBox txtNumofItems;
         private ListBox lstOutput;
-        private Label lblTaxRate;
-        private TextBox txtTaxRate;
         private Button btnCalcPrice;
         private Button btnClear;
         private Button btnQuit;
--- RichardP8Grocery/Form1.cs
+++ RichardP8Grocery/Form1.cs
@@ -6,5 +6,23 @@ namespace RichardP8Grocery
         {
             InitializeComponent();
         }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtItemCost.Clear();
+            txtNumofItems.Clear();
+            lstOutput.Items.Clear();
+
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalcPrice_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
