f6c912356767ee2f4ebc3323b998bbf66d5c79a8
Richard0725 <Richardmunoz25@hotmail.com>
2024-09-23 12:00:45 -0400

    Wrote more code towards Calculate Final Price.

--- RichardP8Grocery/Form1.cs
+++ RichardP8Grocery/Form1.cs
@@ -14,9 +14,6 @@ namespace RichardP8Grocery
             txtItemCost.Clear();
             txtNumofItems.Clear();
             lstOutput.Items.Clear();
-
-            //this changes the focus of the clear button
-            // btnClear.Focus();
             txtItemCost.Focus();
         }
 
@@ -27,16 +24,25 @@ namespace RichardP8Grocery
 
         private void btnCalcPrice_Click(object sender, EventArgs e)
         {
-            // input
+            double taxRate = .0875;
+            double price, totalPrice, taxAmount;
 
+            // input
+            price = double.Parse(txtItemCost.Text);
 
 
             // processing
-
-
+            taxAmount = price * taxRate;
+            totalPrice = taxAmount + price;
 
             // output
+            lstOutput.Items.Add("Price is " + price.ToString("C2"));
+            lstOutput.Items.Add("Tax Rate is " + taxRate.ToString("P2"));
+            lstOutput.Items.Add("Tax amount is " + taxAmount.ToString("C2"));
+            lstOutput.Items.Add("Total Price is " + totalPrice.ToString("C2"));
 
+            //this changes the focus of the clear button
+            btnClear.Focus();
         }
 
         private void txtItemCost_Enter(object sender, EventArgs e)
